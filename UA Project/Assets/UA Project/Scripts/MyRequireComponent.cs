8dd2b2b531f3e76ab5f2c9207320ceba0ca807ed
lPinchol <pinchodev91@gmail.com>
2016-08-22 04:47:03 +0200

    Add RequireComponent

---
+++ UA Project/Assets/UA Project/Scripts/MyRequireComponent.cs
@@ -0,0 +1,13 @@
+﻿/// MyRequireComponent.cs Agosto 22/2016 MoonPincho
+/// 
+
+using UnityEngine;
+
+namespace MoonPincho
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class MyRequireComponent : MonoBehaviour
+    {
+
+    }
+}
