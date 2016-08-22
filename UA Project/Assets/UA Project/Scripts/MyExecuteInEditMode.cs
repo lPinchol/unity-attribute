8c1d5177cd550515db16829cf10808ffa2b8925f
lPinchol <pinchodev91@gmail.com>
2016-08-22 04:56:11 +0200

    Add more att and init first property

---
+++ UA Project/Assets/UA Project/Scripts/MyExecuteInEditMode.cs
@@ -0,0 +1,19 @@
+﻿/// MyExecuteInEditMode.cs Agosto 22/2016 MoonPincho
+/// 
+
+using UnityEngine;
+
+namespace MoonPincho
+{
+    [ExecuteInEditMode]
+    public class MyExecuteInEditMode : MonoBehaviour
+    {
+        private void Update()
+        {
+            gameObject.GetComponent<Renderer>().sharedMaterial.color = new Color(Random.Range(0f, 1f),
+                                                                                 Random.Range(0f, 1f),
+                                                                                 Random.Range(0f, 1f),
+                                                                                 Random.Range(0f, 1f));
+        }
+    }
+}
