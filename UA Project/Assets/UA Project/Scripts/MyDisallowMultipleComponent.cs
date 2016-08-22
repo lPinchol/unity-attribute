fac83edf8ee901f2231fe8c96014b90bff5aea10
lPinchol <pinchodev91@gmail.com>
2016-08-22 04:49:15 +0200

    Add DisallowMultipleComponent

---
+++ UA Project/Assets/UA Project/Scripts/MyDisallowMultipleComponent.cs
@@ -0,0 +1,12 @@
+﻿/// MyDisallowMultipleComponent.cs Agosto 22/2016 MoonPincho
+/// 
+
+using UnityEngine;
+
+namespace MoonPincho
+{
+    [DisallowMultipleComponent]
+    public class MyDisallowMultipleComponent : MonoBehaviour {
+
+    }
+}
