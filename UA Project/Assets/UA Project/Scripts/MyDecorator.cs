8bcfd8a0d17193811d9acfd9a2882d0a08626f59
lPinchol <pinchodev91@gmail.com>
2016-08-22 05:16:40 +0200

    Add MenuItem

--- UA Project/Assets/UA Project/Scripts/MyDecorator.cs
+++ UA Project/Assets/UA Project/Scripts/MyDecorator.cs
@@ -1,51 +1,57 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/// MyDecorator.cs Agosto 22/2016 MoonPincho
+/// 
 
-public class MyDecorator : MonoBehaviour {
+using UnityEngine;
 
-    [SerializeField]
-    private string myName = "Need Name";
-
-    [HideInInspector]
-    public string desc = "Here is a public string that I wnat to hide in the inspector.";
-
-    [TextArea]
-    public string editBox = "";
-
-    [Multiline(10)]
-    public string editBox2 = "";
-
-    public const float minHealth = 0f;
-    public const float maxHealth = 200f;
-
-    [Range(minHealth,maxHealth)]
-    public int health;
-
-    [Header("Currency")]
-    public int gold;
-    public int silver;
-    public int copper;
-
-    [Header("Stats")]
-    public int strength;
-    public int dexterity;
-    public int constitution;
-
-    [Space(10)]
-    public string race;
-    public int age;
-
-    [Space(10)]
-    [Tooltip("This is a objects position in the game world")]
-    public Vector3 vec3;
-
-    [Space(10)]
-    [IntRangeAttribute(0,10)]
-    public IntRange randomInt;
-
-    private void Update()
+namespace MoonPincho
+{
+    public class MyDecorator : MonoBehaviour
     {
-        int num = randomInt;
-        Debug.Log(num);
+
+        [SerializeField]
+        private string myName = "Need Name";
+
+        [HideInInspector]
+        public string desc = "Here is a public string that I wnat to hide in the inspector.";
+
+        [TextArea]
+        public string editBox = "";
+
+        [Multiline(10)]
+        public string editBox2 = "";
+
+        public const float minHealth = 0f;
+        public const float maxHealth = 200f;
+
+        [Range(minHealth, maxHealth)]
+        public int health;
+
+        [Header("Currency")]
+        public int gold;
+        public int silver;
+        public int copper;
+
+        [Header("Stats")]
+        public int strength;
+        public int dexterity;
+        public int constitution;
+
+        [Space(10)]
+        public string race;
+        public int age;
+
+        [Space(10)]
+        [Tooltip("This is a objects position in the game world")]
+        public Vector3 vec3;
+
+        [Space(10)]
+        [IntRangeAttribute(0, 10)]
+        public IntRange randomInt;
+
+        private void Update()
+        {
+            int num = randomInt;
+            Debug.Log(num);
+        }
     }
 }
---
+++ UA Project/Assets/UA Project/Scripts/MyMenuItem.cs
@@ -0,0 +1,23 @@
+﻿/// MyMenuItem.cs Agosto 22/2016 MoonPincho
+/// 
+
+using UnityEngine;
+using UnityEditor;
+
+namespace MoonPincho
+{
+    public class MyMenuItem : MonoBehaviour
+    {
+        [MenuItem("MoonPincho/My Custom SubMenu/My Custom Menu Item #%w")]
+        private static void Init()
+        {
+            Debug.Log("Init was called");
+        }
+
+        [MenuItem("CONTEXT/Rigidbody/My Rigidbody Debug")]
+        private static void RigidbodyDebug()
+        {
+            Debug.Log("RigidbodyDebug was called on: " + Selection.activeTransform.gameObject.name);
+        }
+    }
+}
